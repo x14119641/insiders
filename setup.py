b79020120a8025d00ef0a6564cfb354789813804
Dani <dgilromero@bloomberg.net>
2019-04-23 11:16:49 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+with open('README.md', 'r') as fr:
+    long_description = fr.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read()
+
+
+setup(name='InisdersScraper',
+      version='1.0',
+      description='Scraper for get insiders data from "gurufocus"',
+      author='Dani',
+      author_email='daniel.gil.romero24@gmail.com',
+      packages=['lib', 'tests', ],
+      install_requires=requirements,
+      url='https://github.com/x14119641/insiders',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license='MIT',
+      )
